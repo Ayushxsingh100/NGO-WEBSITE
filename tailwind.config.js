6ac56f4a9f26b23070905d8e51c598eb20939d89
Ayush Kumar Singh <ayushxsingh100@outlook.com>
2025-06-27 15:44:20 +0530

    27-06-2025

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lavender: 'rgb(189,147,211)',
+      },
+    },
+  },
+  plugins: [],
+};
